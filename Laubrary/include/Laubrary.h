8cd224ce28cdccee07156005bf6d8492a5622570
Eugenio Marinetto <nenetto@gmail.com>
2015-02-17 17:54:01 +0100

    Laubrary V0.0

---
+++ Laubrary/include/Laubrary.h
@@ -0,0 +1,21 @@
+#ifndef LAUBRARY_H
+#define LAUBRARY_H
+
+
+#include "LaubraryConfig.h"
+#include "NPTrackingTools.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+
+
+namespace Laubrary{
+
+    // Example function
+    void message(); 
+}
+
+
+#endif
---
+++ Laubrary/source/Laubrary.cpp
@@ -0,0 +1,5 @@
+#import "Laubrary.h"
+
+void Laubrary::message(){
+    fprintf(stdout, "[Laubrary]: This is a test message....\n\n");
+}
