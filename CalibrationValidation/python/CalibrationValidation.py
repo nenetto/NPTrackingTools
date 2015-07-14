64f515357562437270ef3621afdb4be92b504db0
nenetto <nenetto@gmail.com>
2015-07-14 16:43:56 +0200

    Example loading Ui

--- CalibrationValidation/python/CalibrationValidation.py
+++ CalibrationValidation/python/CalibrationValidation.py
@@ -6,5 +6,21 @@
 @date 2015-07-14
 '''
 
-import pyqt
+from PySide import QtCore, QtGui, QtUiTools
 
+
+def loadUiWidget(uifilename, parent=None):
+    loader = QtUiTools.QUiLoader()
+    uifile = QtCore.QFile(uifilename)
+    uifile.open(QtCore.QFile.ReadOnly)
+    ui = loader.load(uifile, parent)
+    uifile.close()
+    return ui
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = loadUiWidget("CalibrationTest.ui")
+    MainWindow.show()
+    sys.exit(app.exec_())
