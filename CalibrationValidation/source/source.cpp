dd101a20037f9fb211c831faf65fcbe7f5cb70a9
DavidGarciaMato <100293909@alumnos.uc3m.es>
2015-07-16 18:00:05 +0200

    CameraValidation-source.cpp updated

--- CalibrationValidation/source/source.cpp
+++ CalibrationValidation/source/source.cpp
@@ -27,46 +27,57 @@ int main(int argc, char *argv[])
 	{
 
 		// Read parameters
-
 		std::string calFile(argv[1]);
 		int Thr = atoi(argv[2]);
 		int Exp = atoi(argv[3]);
 		int Led = atoi(argv[4]);
 		std::string resultFile(argv[5]);
 		
-
 		fprintf(stdout, "Calibration File: %s\n", calFile.c_str());
 		fprintf(stdout, "Result File: %s\n", resultFile.c_str());
-
 
 		// Create Tracker
 		Optitrack::OptitrackTracker::Pointer objTracker = Optitrack::OptitrackTracker::New();
 		
-
 		// Load Calibration File
 		objTracker->SetCalibrationFile(calFile);
 		
+		int result = objTracker->Close();
+		std::cout << " Close result: " << result << std::endl;
+		std::cout << " State -> " << objTracker->GetState() << std::endl;
 
 		// Open Connection
-		objTracker->Open();
-		objTracker->LoadCalibration();
+		result = objTracker->Open();
+		std::cout << " Open result: " << result << std::endl;
+		std::cout << " State -> " << objTracker->GetState() << std::endl;
+
+		result = objTracker->LoadCalibration();
+		std::cout << " LoadCalibration result: " << result << std::endl;
+		std::cout << " State -> " << objTracker->GetState() << std::endl;
+
+		int numCam = objTracker->GetCameraNumber();
 
 		// Set Camera Params
-		objTracker->SetCameraParams(Exp, Thr, Led);
-
+		//objTracker->SetCameraParams(Exp, Thr, Led);
+		result = objTracker->SetCameraParams(Exp, Thr, Led, 4);
+		std::cout << " SetCamParams result: " << result << std::endl;
+		
 		// Testing Number of viewed markers
-		int result = objTracker->CheckNumberOfMarkers();
+		result = objTracker->CheckNumberOfMarkers();
 
 		if (result == 1)
 		{
 			fprintf(stdout, "SUCCESS for one marker\n");
 			objTracker->TestCalibration(resultFile);
 		}
-
+		
+		result = objTracker->Close();
+		std::cout << " Close result: " << result << std::endl;
+		std::cout << " State -> " << objTracker->GetState() << std::endl;
 
 	}
 
-
+	system("PAUSE");
     return 0;
 }
 
